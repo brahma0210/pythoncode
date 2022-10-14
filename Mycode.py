f01b16cc35c9a35ff9cb56c90752dc57354cbf8d
Brahma reddy Kakunuri <90281219+brahma0210@users.noreply.github.com>
2022-10-14 04:11:06 -0400

    Add files via upload
    getting an array elements from the user

---
+++ Mycode.py
@@ -0,0 +1,8 @@
+from array import *
+val = array('i',[ ])
+n = int(input("enter the length of an array :"))
+print()
+for i in range(n):
+    x =int(input("enter the next array"))
+    val.append(x)
+print(val) 
